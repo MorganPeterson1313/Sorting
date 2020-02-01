802fe835db793020cf74eccaa85faef40c1ce884
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2020-02-01 12:04:57 -0500

    working on passing all the test

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,12 +6,16 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
+    for j in range(cur_index, len(arr)):
+        if arr[smallest_index] >arr[j]:
+            smallest_index = j
 
 
 
         # TO-DO: swap
-
+        temp = arr[smallest_index]
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = temp
 
 
 
@@ -20,6 +24,18 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    smallest_index = 0
+    cur_index = smallest_index + smallest_index[1]
+    swap_flag = False
+    while swap_flag == True:
+        swap_flag == False
+    for j in range( cur_index, len(arr[smallest_index] + 1)):
+        if j > arr[smallest_index]:
+            arr[smallest_index] = arr[cur_index]
+            swap_flag == True
+
+
+
 
     return arr
 
